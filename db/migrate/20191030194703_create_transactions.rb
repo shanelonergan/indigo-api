eda31d38769cf609c413c0a8324134ffbc4c2712
shanelonergan <sptlonergan@gmail.com>
2019-10-30 15:47:38 -0400

    created user, favorite, and transaction models

---
+++ db/migrate/20191030194703_create_transactions.rb
@@ -0,0 +1,12 @@
+class CreateTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transactions do |t|
+      t.bigint :buyer_id
+      t.bigint :seller_id
+      t.references :listing, null: false, foreign_key: true
+      t.float :final_price
+
+      t.timestamps
+    end
+  end
+end
