85c002e293da7eeddb2e87044139c41b8516c949
shanelonergan <sptlonergan@gmail.com>
2019-10-31 12:40:59 -0400

    fixed minor auth synthax

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,7 +4,7 @@ class AuthController < ApplicationController
         user = User.find_by(username: user_params[:username])
         if user && user.authenticate(user_params[:password])
             payload = {user_id: user.id}
-            token = JWT.encode(payload, secret, true, {alogorithm: 'HS256'})
+            token = JWT.encode(payload, secret, 'HS256')
             render json: {user: user. token: token}
         else
             render json: {errors: user.errors.full_messages}
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   resources :users
 
   get '/persist' to: 'auth#persist'
-  get '/login' to; 'auth#login'
+  post '/login' to; 'auth#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
