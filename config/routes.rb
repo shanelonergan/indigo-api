536f9c362ac66b81115b55209f6c878b88a3188b
shanelonergan <sptlonergan@gmail.com>
2019-11-01 16:23:34 -0400

    fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :favorites
   resources :users
 
-  get '/persist' to: 'auth#persist'
-  post '/login' to; 'auth#login'
+  get '/persist', to: 'auth#persist'
+  post '/login', to: 'auth#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
