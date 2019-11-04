b415eb6d1bf749a021abbfbe7b8a6ede5f9fc6f3
shanelonergan <sptlonergan@gmail.com>
2019-11-04 14:27:50 -0500

    can create listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,29 @@
 class ListingsController < ApplicationController
+    def create
+        listing = Listing.create(listing_params)
+        if listing.valid?
+            render json: listing
+        else
+            render json: {errors: listing.errors.full_messages}
+        end
+    end
+
     def index
         render json: Listing.all
+    end
+
+    private
+
+    def listing_params
+        params.permit(
+            :name,
+            :category_id,
+            :brand_id,
+            :waist,
+            :length,
+            :weight,
+            :wash_id,
+            :mill_id,
+            :condition_id)
     end
 end
