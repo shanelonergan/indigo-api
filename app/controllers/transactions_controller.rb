743985d0f0643bcac83fb1e0eefa77a6e1ba922c
shanelonergan <sptlonergan@gmail.com>
2019-11-07 17:46:10 -0500

    purchase now creates transaction in db

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,22 @@
 class TransactionsController < ApplicationController
+    def create
+        transaction = Transaction.create(transaction_params)
+        if transaction.valid?
+            render json: transaction
+        else
+            render json: {errors: transaction.errors.full_messages}
+        end
+    end
+
+    private
+
+    def transaction_params
+        params.permit(
+            :buyer_id,
+            :seller_id,
+            :listing_id,
+            :final_price
+        )
+    end
 end
+
