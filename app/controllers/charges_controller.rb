7f858eb27f300ee2c08bc06ca06da002aae3e151
shanelonergan <sptlonergan@gmail.com>
2019-11-08 10:20:39 -0500

    stripe oficially up and running

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,25 +6,45 @@ class ChargesController < ApplicationController
 
     def create
 
+      begin
+
         Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+        token = params[:charge][:token]
+        price = params[:price]
 
         # byebug
 
-        session = Stripe::Checkout::Session.create(
-            payment_method_types: ['card'],
-            line_items: [{
-              name: 'T-shirt',
-              description: 'Comfortable cotton t-shirt',
-              images: ['https://example.com/t-shirt.png'],
-              amount: 500,
-              currency: 'usd',
-              quantity: 1,
-            }],
-            success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url: 'https://example.com/cancel',
-          )
+        charge = Stripe::Charge.create({
+          amount: price * 100,
+          currency: 'usd',
+          description: 'Example charge',
+          source: token,
+        })
 
-          render json: session
+        render json: charge
+
+      rescue Stripe::CardError => e
+        puts "Status is: #{e.http_status}"
+        puts "Type is: #{e.error.type}"
+        puts "Charge ID is: #{e.error.charge}"
+        # The following fields are optional
+        puts "Code is: #{e.error.code}" if e.error.code
+        puts "Decline code is: #{e.error.decline_code}" if e.error.decline_code
+        puts "Param is: #{e.error.param}" if e.error.param
+        puts "Message is: #{e.error.message}" if e.error.message
+
+      rescue Stripe::InvalidRequestError => e
+        render json: e
+      rescue Stripe::AuthenticationError => e
+        render json: e
+      rescue Stripe::APIConnectionError => e
+        render json: e
+      rescue Stripe::StripeError => e
+        render json: e
+      rescue => e
+        render json: e
+      end
+
     end
 
 end
