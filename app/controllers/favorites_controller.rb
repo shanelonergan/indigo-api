bf4c94b374af445966c81510b437c96cf4f53605
shanelonergan <sptlonergan@gmail.com>
2019-11-12 17:01:48 -0500

    added create and index to favorite controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,26 @@
 class FavoritesController < ApplicationController
+
+    def index
+        render json: Favorite.all
+    end
+
+    def create
+        favorite = Favorite.create(favorite_params)
+        # byebug
+        if favorite.valid?
+            render json: favorite
+        else
+            render json: {errors: favorite.errors.full_messages}
+        end
+    end
+
+    private
+
+    def favorite_params
+        params.permit(
+            :user_id,
+            :listing_id
+        )
+    end
+
 end
