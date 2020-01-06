ec171ad0c7eb626684fb62b01f9e22d67817db19
shanelonergan <sptlonergan@gmail.com>
2020-01-06 13:58:07 -0700

    changed cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'indigo-resale.store'
+    origins 'https://indigo-resale.store, https://indigo-deployment.netlify.com'
 
     resource '*',
       headers: :any,
