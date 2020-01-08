d57a797da847ecc8006a1b9c59256d4d911ae8c7
shanelonergan <sptlonergan@gmail.com>
2020-01-07 23:33:01 -0500

    fixing conflicts

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://indigo-resale.store, https://indigo-deployment.netlify.com'
+    # origins 'https://indigo-resale.store, https://indigo-deployment.netlify.com'
+    origins '*'
 
     resource '*',
       headers: :any,
