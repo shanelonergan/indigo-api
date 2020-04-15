1b4cf4df970f48ac36c8d48bc4deee9bdda5e135
shanelonergan <sptlonergan@gmail.com>
2020-04-15 13:18:43 -0400

    destroy method for favorite controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,12 +6,16 @@ class FavoritesController < ApplicationController
 
     def create
         favorite = Favorite.create(favorite_params)
-        # byebug
         if favorite.valid?
             render json: favorite
         else
             render json: {errors: favorite.errors.full_messages}
         end
+    end
+
+    def destroy
+        favorite = Favorite.find_by(user_id: favorite_params[:user_id], listing_id[:listing_id])
+        favorite.destroy
     end
 
     private
